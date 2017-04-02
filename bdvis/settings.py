e8416355f09e72560afe1b34761f1c7f2e2a0aa8
Pongsakorn U-chupala <puchupala@gmail.com>
2017-04-02 07:32:44 -0400

    Allowed all cross-site script

--- bdvis/settings.py
+++ bdvis/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'o7#b(bpie9(8ku7$7$w!26f3^7-1tb4s^t6^5xeyk@wkap=#b0'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["centrahack01.acis.ufl.edu"]
+ALLOWED_HOSTS = ["centrahack01.acis.ufl.edu", "127.0.0.1", "localhost"]
 
 
 # Application definition
@@ -38,12 +38,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # 'django.contrib.gis',
+    'corsheaders',
     'core',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -120,3 +122,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CORS_ORIGIN_ALLOW_ALL = True
