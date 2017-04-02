535966210b7fba1b44a38ac3b27d6368262677c0
Pongsakorn U-chupala <puchupala@gmail.com>
2017-04-01 21:55:29 -0400

    Create Business model
    - Created Business model
    - Imported active business dataset
    - Created import script

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from core.models import Business
 
-# Register your models here.
+
+class BusinessAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Business, BusinessAdmin)
--- core/models.py
+++ core/models.py
@@ -1,5 +1,15 @@
 from __future__ import unicode_literals
 
-from django.db import models
+# from django.db import models
+from django.contrib.gis.db import models
 
-# Create your models here.
+
+class Business(models.Model):
+    name = models.CharField(max_length=255)
+    business_phone = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    location = models.CharField(max_length=255)
+    category = models.CharField(max_length=255)
+    start_date = models.DateField()
+    physical_address = models.TextField()
+    mailing_address = models.TextField()
